528faadc691641295d382411aa45da42726cf1d3
Vaishnavireddiee01 <vanugu47@gmail.com>
2025-04-09 15:52:28 +0530

    how to make a get request and render fixed data in react component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import PostList from './components/PostList';
+
+
+//how to make a get request and render fixed data in react cmpnt
+//to fetch the data we need to have an API endpoint for that i will
+//be making use of json placeholder *
 
 function App() {
   return (
     <div className="App">
-      
+      <PostList />
     </div>
   );
 }
