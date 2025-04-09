01e4c1a8dc39b3cdca521489b13f5b098a82e511
Vaishnavireddiee01 <vanugu47@gmail.com>
2025-04-09 18:10:56 +0530

    HTTP POST REQUEST

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import PostList from './components/PostList';
+import PostForm from './components/PostForm';
 
 
 //how to make a get request and render fixed data in react cmpnt
@@ -11,7 +12,10 @@ import PostList from './components/PostList';
 function App() {
   return (
     <div className="App">
+      {/*http get request*/}
       <PostList />
+      {/*http post request*/}
+      <PostForm />
     </div>
   );
 }
